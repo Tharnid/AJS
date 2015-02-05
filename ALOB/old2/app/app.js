1702db6de01d2f5bc80c5d4380f93560f0614ef8
Tharnid <jeffmsmithdev@gmail.com>
2015-02-05 00:05:10 -0500

    had to modify some files

---
+++ ALOB/old2/app/app.js
@@ -0,0 +1,37 @@
+(function () {
+    "use strict";
+    var app = angular.module("productManagement",
+                            ["common.services",
+                             "ui.router",
+                             "productResourceMock"]);
+
+    app.config(["$stateProvider",
+                "$urlRouterProvider",
+        function($stateProvider, $urlRouterProvider){
+            $urlRouterProvider.otherwise("/");  // default Url
+
+            $stateProvider
+                .state("home", {
+                    url: "/",
+                    templateUrl: "app/welcomeView.html"
+                })
+
+                // Products
+                .state("productList", {
+                    url: "/products",
+                    templateUrl: "app/products/productListView.html",
+                    controller: "ProductListCtrl as vm"
+                })
+                .state("productEdit", {
+                    url: "/products/edit/:productId",
+                    template: "app/products/productEditView.html", 
+                    controller: "ProductEditCtrl as vm"
+                })
+                .state("productDetail", {
+                    url: "/products/:productId", 
+                    templateUrl: "app/products/productDetailView.html", 
+                    controller: "ProductDetailCtrl as vm"
+                })
+        }]
+    );
+}());
