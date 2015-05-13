048c535b7a8f5d78d811530e6a22151573d4f2b3
Tharnid <jeffmsmithdev@gmail.com>
2015-05-12 23:14:34 -0400

    done with routes

--- TharnRest/s3/Routes/bookRoutes.js
+++ TharnRest/s3/Routes/bookRoutes.js
@@ -31,16 +31,30 @@ var routes = function(Book) {
 
     bookRouter.route('/Books/:bookId')
         .get(function(req,res){
-
-
             Book.findById(req.params.bookId, function(err,book){
                 if(err)
                     res.status(500).send(err);
                 else
                     res.json(book);
-            });
+            })
+                .put(function(req, res) {
+                   Book.findById(req.params.bookId, function(err, book){
+                      if(err)
+                        res.status(500).send(err);
+                       else
+                            req.book.title = req.body.title;
+                            req.book.author = req.body.author;
+                            req.book.genre = req.body.genre;
+                            req.book.read = req.body.read;
+                            book.save();
+                        res.json(book);
+                   });
+                });
         });
     return bookRouter;
 };
 
 module.exports = routes;
+
+// Patch updates part of an item
+// Put updates a whole item
